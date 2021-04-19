a6734e746e203dfd1750e1acea7797231273a702
2y2son4 <adrianadelteso@gmail.com>
2021-04-19 16:45:56 +0200

    :sparkles: Add view and download image functionalities

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 const canvas = document.querySelector('#draw');
 const reset = document.querySelector('.reset');
+const view = document.querySelector('.view');
+const downloader = document.getElementById('downloader');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth - 200;
@@ -23,7 +25,7 @@ let direction = true;
 
 function getCoords(ev) {
   const boundingBox = canvas.getBoundingClientRect();
-  console.log(boundingBox);
+  // console.log(boundingBox);
   const { left, top } = boundingBox;
   return [ev.clientX - left, ev.clientY - top];
 }
@@ -66,8 +68,25 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => (isDrawing = false));
 canvas.addEventListener('mouseout', () => (isDrawing = false));
 
+// reset btn
 function handleReset() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 reset.addEventListener('click', handleReset);
+
+// view image btn
+function viewImage() {
+  const img = canvas.toDataURL('image/png');
+  const w = window.open('about:blank', 'image from canvas');
+  w.document.write("<img src='" + img + "' alt='from canvas'/>");
+}
+view.addEventListener('click', viewImage);
+
+// download image btn
+function downloadImage() {
+  downloader.download = 'image.png';
+  downloader.href = canvas.toDataURL('image/png').replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+}
+
+downloader.addEventListener('click', downloadImage);
